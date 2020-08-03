c1218eb7ab093a31767d5f995a01e0e719c8a720
Daniel Leal <os.leal90@gmail.com>
2020-08-03 17:27:29 -0300

    feat web interface

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PageHeader from '../../components/PageHeader'
+
+export default function TeacherForm() {
+  return(
+    <div>
+      <PageHeader title="Que incrível que você quer dar aulas." />
+      <h1>TeacherForm</h1>
+    </div>
+  )
+}
