90c55b757f01798b31eb058f8f65a7d5f9ac2c3f
Daniel Leal <os.leal90@gmail.com>
2020-08-05 18:30:44 -0300

    add api connection

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+
+import api from '../../services/api'
 
 import './styles.css'
 import logoImg from '../../assets/images/logo.svg'
@@ -9,6 +11,16 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 
 export default function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0)
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data
+      setTotalConnections(total)
+    })
+  }, [])
+
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -32,7 +44,7 @@ export default function Landing() {
           </Link>
         </div>
         <span className="total-connections">
-          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+          Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
         </span>
       </div>
     </div>
